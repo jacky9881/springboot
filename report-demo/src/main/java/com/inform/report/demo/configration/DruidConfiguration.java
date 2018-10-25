23243cc982a85bf875f2993a4be88a38e96eb18f
jacky9881 <jacky9881@163.com>
2018-10-25 15:57:08 +0800

    学习项目初始化

---
+++ report-demo/src/main/java/com/inform/report/demo/configration/DruidConfiguration.java
@@ -0,0 +1,51 @@
+package com.inform.report.demo.configration;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+
+@Configuration
+public class DruidConfiguration {
+	
+	@Bean(destroyMethod = "close", initMethod = "init")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        return druidDataSource;
+    }
+	
+	@Bean
+	public ServletRegistrationBean registrationBean() {
+		ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        /** 初始化参数配置，initParams**/
+        //白名单
+        bean.addInitParameter("allow", "127.0.0.1");
+        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        bean.addInitParameter("deny", "192.168.1.73");
+        //登录查看信息的账号密码.
+        bean.addInitParameter("loginUsername", "admin");
+        bean.addInitParameter("loginPassword", "admin");
+        //是否能够重置数据.
+        bean.addInitParameter("resetEnable", "false");
+        return bean;
+	}
+	
+	@Bean
+    public FilterRegistrationBean druidStatFilter() {
+        FilterRegistrationBean bean = new FilterRegistrationBean(new WebStatFilter());
+        //添加过滤规则.
+        bean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息.
+        bean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico");
+        return bean;
+    }
+	
+}
